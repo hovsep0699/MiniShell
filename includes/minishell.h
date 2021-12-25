a50b1a8cbc50e716da0eeea01afd9a569e27415f
TUMO Labs <user@TUMOs-iMac.local>
2021-12-25 21:29:35 +0400

    add subheader file

--- includes/minishell.h
+++ includes/minishell.h
@@ -30,48 +30,9 @@
 # include <readline/history.h>
 # include <limits.h>
 # include <sys/ioctl.h>
+# include "shell_defines.h"
 
 
-# define MAX_EXIT_STATUS 255
-# define START 0
-# define PARSER_ERROR -1
-# define WRONG_INPUT -2
-# define COMAND_NOT_FOUND -3
-# define BAD_ASSIGNMENT -4
-# define COMMAND_NOT_FOUND 127
-# define UNPRINT_ERROR -10
-# define INVAILDPARAMETERNAME -6
-# define FIRSTDIGIT -8
-# define SUCCESS 1
-# define CHILD_PROC 0
-# define PATH_SHOW_NUMBER 3
-# define GREATHER 1
-# define OPEN_ANGEL_BRACKETS 2
-# define OPEN_CLOSE 4
-# define DGREATHER 5
-# define NOT_NEW_LINE 6
-# define CHAR_QUATES 39
-# define CHAR_DQUATES 34
-# define TEXT_RED "\033[0;31m"
-# define TEXT_BLACK "\033[0;30m"
-# define TEXT_GREEN "\033[0;32m"
-# define TEXT_YELLOW "\033[0;33m"
-# define TEXT_BLUE "\033[0;34m"
-# define TEXT_PURPLE "\033[0;35m"
-# define TEXT_CYAN "\033[0;34m"
-# define TEXT_WHITE "\033[0;37m"
-# define SIGHUP  1
-# define SIGINT  2
-# define SIGQUIT 3
-# define SIGILL  4
-# define SIGTRAP 5
-# define SIGABRT 6
-# define STDIN_FILE 0
-# define STDOUT_FILE 1
-# define STDERR_FILE 2
-# define MAX_BUILTIN_FUNCS 10
-# define MAX_SIDE_FUNCS 6
-# define MAX_FRONT_FUNCS 3
 
 typedef struct s_dictionary				t_dictionary;
 typedef struct s_front_function			t_front_function;
